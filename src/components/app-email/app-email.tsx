84d09d8a4d75d663ef7688fa0c4779e91d7c03e0
Stephen Ward <srmward@gmail.com>
2018-06-16 16:56:28 -0700

    Include Form, State, Email Capture Build

--- src/components/app-email/app-email.tsx
+++ src/components/app-email/app-email.tsx
@@ -1,18 +1,52 @@
-import { Component } from '@stencil/core'
+import { Component, State } from '@stencil/core'
 
 @Component({
   tag: 'app-email',
   styleUrl: 'app-email.scss',
 })
 export class AppEmail {
+  @State() value: string
+  @State() selectValue: string
+
+  handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  handleChange(e) {
+    this.value = e.target.value
+
+    if (e.target.validity.typeMismatch) {
+    }
+  }
+
+  handleSelect(e) {
+    this.selectValue = e.target.value
+  }
+
   render() {
     return (
-      <div class="app-email">
-        <p>email section</p>
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={this.value}
+            onInput={e => this.handleChange(e)}
+          />
+        </label>
+
+        <select value={this.selectValue} onInput={e => this.handleSelect(e)}>
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+          <option value="both">Both ;D</option>
+        </select>
+
+        <input type="submit" value="Submit" />
+
         <stencil-route-link url="/">
           <button>Back</button>
         </stencil-route-link>
-      </div>
+      </form>
     )
   }
 }
